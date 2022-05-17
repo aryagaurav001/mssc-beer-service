38bfd44d01619f319f2f7774965e2a33b5390fc5
aryagaurav001 <aryagaurav_dce_cs@yahoo.co.in>
2022-05-17 07:35:48 +0530

    Adding circleci config. Closes #11

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-17T07:04:16+0530",
+    date = "2022-05-17T07:13:29+0530",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.14 (Oracle Corporation)"
 )
 @Component
