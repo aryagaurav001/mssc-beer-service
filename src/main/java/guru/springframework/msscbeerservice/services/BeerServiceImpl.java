00391b31d08cc9b48fce5a67b943d6ea76444789
aryagaurav001 <aryagaurav_dce_cs@yahoo.co.in>
2022-07-26 23:19:35 +0530

    Added endpoint to get beer by upc. closes #22

--- src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -96,4 +96,10 @@ public class BeerServiceImpl implements BeerService {
 
         return beerPagedList;
     }
+
+    @Cacheable(cacheNames = "beerUpcCache")
+    @Override
+    public BeerDto getByUpc(String upc) {
+        return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
+    }
 }
