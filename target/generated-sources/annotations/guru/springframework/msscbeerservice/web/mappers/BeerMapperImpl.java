cd7126a7be29364cb324b74f3940c9fe3b80d048
aryagaurav001 <aryagaurav_dce_cs@yahoo.co.in>
2022-08-20 08:22:27 +0530

    Configured spring profile for local mysql. closes #25

---
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-08-20T07:46:00+0530",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.14 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
